79e27e6ae3cb55b46ab45852894239b37421fedb
Daniel Magers <daniel.magers@gmail.com>
2022-08-28 07:50:41 -0500

    commit tree 4e79de2f872d6720281b2e0a009779546cba14cf

--- methods.rb
+++ methods.rb
@@ -1,1 +1,120 @@
 # Your code here!
+
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+
+
+# def reverse_name(name)
+#     if name.class != String
+#         return nil
+#     end
+
+#     name.reverse
+# end
+
+def halve (number)
+    if number.class !=integer
+        return nil
+    end
+
+    number / 2 
+end
+
+
+
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+# end
+
+# def say_hello()
+#     "hello!"
+# end
+
+# def say_hello
+#     "hello"
+# end
+
+
+# def say_hi(name = "Rubyist")
+#     puts "Hi there, #{name}!"
+# end
+
+# def add_and_log(num1, num2)
+#     puts num1 + num2
+# end
+
+# def add_and_return(num1, num2)
+#     return num1 + num2
+# end 
+
+# sum1 = add_and_log(2, 2)
+# sum2 = add_and_return(2,2)
+
+# def print_name
+#     puts "Bob Ross"
+# end
+
+# def return_name
+#     "Bob Ross"
+# end
+
+# def print_and_return_name
+#     "Bob Ross"
+#     puts "Bob Ross"
+# end
+
+# def stylish_painter
+#     best_hairstyle = "Bob Ross"
+#     "Jean-Michel Basquiat"
+#     best_hairstyle
+# end
+
+# def reverse_name(name)
+#     if name.class != String
+#         return nil
+#     end
+
+#     name.reverse
+# end
+
